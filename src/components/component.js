12180e9a4da7de822bdb85ba6f7f2905ac07fa79
Caleb Nwaizu <calebchris000@gmail.com>
2023-03-21 07:39:13 +0100

    Change dig to digit

--- src/components/component.js
+++ src/components/component.js
@@ -1,8 +1,8 @@
 import './component.css';
 
 function Buttons(prop) {
-  const { dig } = prop;
-  return <div className="button">{dig}</div>;
+  const { digit } = prop;
+  return <div className="button">{digit}</div>;
 }
 
 function CalculatorUI() {
@@ -10,25 +10,25 @@ function CalculatorUI() {
     <div className="container">
       <div className="screen"><p className="number">0</p></div>
       <div className="buttons">
-        <Buttons dig="AC" />
-        <Buttons dig="±" />
-        <Buttons dig="%" />
-        <Buttons dig="÷" />
-        <Buttons dig="7" />
-        <Buttons dig="8" />
-        <Buttons dig="9" />
-        <Buttons dig="×" />
-        <Buttons dig="4" />
-        <Buttons dig="5" />
-        <Buttons dig="6" />
-        <Buttons dig="-" />
-        <Buttons dig="1" />
-        <Buttons dig="2" />
-        <Buttons dig="3" />
-        <Buttons dig="+" />
-        <Buttons dig="0" />
-        <Buttons dig="." />
-        <Buttons dig="=" />
+        <Buttons digit="AC" />
+        <Buttons digit="±" />
+        <Buttons digit="%" />
+        <Buttons digit="÷" />
+        <Buttons digit="7" />
+        <Buttons digit="8" />
+        <Buttons digit="9" />
+        <Buttons digit="×" />
+        <Buttons digit="4" />
+        <Buttons digit="5" />
+        <Buttons digit="6" />
+        <Buttons digit="-" />
+        <Buttons digit="1" />
+        <Buttons digit="2" />
+        <Buttons digit="3" />
+        <Buttons digit="+" />
+        <Buttons digit="0" />
+        <Buttons digit="." />
+        <Buttons digit="=" />
       </div>
     </div>
   );
