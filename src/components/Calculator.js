4d7d86505ea2401fe765ef08bdedcebfb145a954
Caleb Nwaizu <calebchris000@gmail.com>
2023-03-23 06:51:04 +0100

    Fix linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -20,34 +20,37 @@ function CalculatorUI() {
   }
 
   return (
-    <div className="container">
-      <div className="screen">
+    <div className="main">
+      <div className="container">
+        <div className="screen">
 
-        <p className="number">{data.next || data.total || '0'}</p>
+          <p className="number">{data.next || data.total || '0'}</p>
+        </div>
+        <div className="buttons">
+          <Buttons digit="AC" name="AC" />
+          <Buttons digit="±" name="+/-" />
+          <Buttons digit="%" name="%" />
+          <Buttons digit="÷" name="÷" />
+          <Buttons digit="7" name="7" />
+          <Buttons digit="8" name="8" />
+          <Buttons digit="9" name="9" />
+          <Buttons digit="×" name="x" />
+          <Buttons digit="4" name="4" />
+          <Buttons digit="5" name="5" />
+          <Buttons digit="6" name="6" />
+          <Buttons digit="-" name="-" />
+          <Buttons digit="1" name="1" />
+          <Buttons digit="2" name="2" />
+          <Buttons digit="3" name="3" />
+          <Buttons digit="+" name="+" />
+          <Buttons digit="0" name="0" />
+          <Buttons digit="." name="." />
+          <Buttons digit="=" name="=" />
+        </div>
+        <CommentSpace />
       </div>
-      <div className="buttons">
-        <Buttons digit="AC" name="AC" />
-        <Buttons digit="±" name="+/-" />
-        <Buttons digit="%" name="%" />
-        <Buttons digit="÷" name="÷" />
-        <Buttons digit="7" name="7" />
-        <Buttons digit="8" name="8" />
-        <Buttons digit="9" name="9" />
-        <Buttons digit="×" name="x" />
-        <Buttons digit="4" name="4" />
-        <Buttons digit="5" name="5" />
-        <Buttons digit="6" name="6" />
-        <Buttons digit="-" name="-" />
-        <Buttons digit="1" name="1" />
-        <Buttons digit="2" name="2" />
-        <Buttons digit="3" name="3" />
-        <Buttons digit="+" name="+" />
-        <Buttons digit="0" name="0" />
-        <Buttons digit="." name="." />
-        <Buttons digit="=" name="=" />
-      </div>
-      <CommentSpace />
     </div>
+
   );
 }
 
